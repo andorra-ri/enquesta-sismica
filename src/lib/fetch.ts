23a2af4708fbe8f44311c4ec125c5f2318d804e4
Roger Veciana <roger.veciana@abzu.ai>
2023-10-01 23:16:53 +0200

    Start reports part

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,113 +1,133 @@
-import { createClient } from "@supabase/supabase-js";
-import { createXMLReport, getIndices } from "./index-functions";
-import type { FormValues } from "./store";
+import { createClient } from '@supabase/supabase-js';
+import { createXMLReport, getIndices } from './index-functions';
+import type { FormValues } from './store';
 
-const supabaseUrl = "https://svyhccttsxgjrefxiiro.supabase.co";
+const supabaseUrl = 'https://svyhccttsxgjrefxiiro.supabase.co';
 const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzOTA4ODc0NiwiZXhwIjoxOTU0NjY0NzQ2fQ.XmlC695aKtt85caWPcPSDWBUP9z4RDS_eGQ-eWjfwKE";
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzOTA4ODc0NiwiZXhwIjoxOTU0NjY0NzQ2fQ.XmlC695aKtt85caWPcPSDWBUP9z4RDS_eGQ-eWjfwKE';
 
 export interface SeismsData {
-  guid: string;
-  datetime: Date;
-  magnitude: number;
-  region: string;
-  lon: string;
-  lat: string;
+	guid: string;
+	datetime: Date;
+	magnitude: number;
+	region: string;
+	lon: string;
+	lat: string;
 }
 
 export const getSeismData = async () => {
-  const supabaseClient = createClient(supabaseUrl, supabaseAnonKey, {
-    db: {
-      schema: "seismology",
-    },
-  });
+	const supabaseClient = createClient(supabaseUrl, supabaseAnonKey, {
+		db: {
+			schema: 'seismology'
+		}
+	});
 
-  const now = new Date();
-  const fifteenDaysAgo = new Date(
-    now.getFullYear(),
-    now.getMonth(),
-    now.getDate() - 15
-  );
+	const now = new Date();
+	const fifteenDaysAgo = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 15);
 
-  const { data: dataSeism, error: errorSeism } = await supabaseClient
-    .from("last_seisms")
-    .select()
-    .gt("datetime", fifteenDaysAgo.toISOString().split("T")[0]);
+	const { data: dataSeism, error: errorSeism } = await supabaseClient
+		.from('last_seisms')
+		.select()
+		.gt('datetime', fifteenDaysAgo.toISOString().split('T')[0]);
 
-  errorSeism && console.log("Error downloading seisms:", errorSeism);
-  return (dataSeism ?? []) as SeismsData[];
+	errorSeism && console.log('Error downloading seisms:', errorSeism);
+	return (dataSeism ?? []) as SeismsData[];
 };
 
 export interface TerritoriData {
-  nom: string;
+	nom: string;
 }
 export interface ParroquiaData {
-  parroquia: string;
-  territori: TerritoriData[];
+	parroquia: string;
+	territori: TerritoriData[];
 }
 
 export const getParroquiesData = async () => {
-  const supabaseClientPublic = createClient(supabaseUrl, supabaseAnonKey);
+	const supabaseClientPublic = createClient(supabaseUrl, supabaseAnonKey);
 
-  const { data: dataMunicipalities, error: errorMunicipalities } =
-    await supabaseClientPublic.from("parroquia").select(`parroquia,
+	const { data: dataMunicipalities, error: errorMunicipalities } = await supabaseClientPublic.from(
+		'parroquia'
+	).select(`parroquia,
     territori!fk_parroquia (nom)`);
 
-  errorMunicipalities &&
-    console.log("Error downloading municipalities:", errorMunicipalities);
-  return dataMunicipalities as ParroquiaData[];
+	errorMunicipalities && console.log('Error downloading municipalities:', errorMunicipalities);
+	return dataMunicipalities as ParroquiaData[];
 };
 
 export const sendToSupabase = async (data: FormValues) => {
-  const supabaseClient = createClient(supabaseUrl, supabaseAnonKey, {
-    db: {
-      schema: "seismology",
-    },
-  });
+	const supabaseClient = createClient(supabaseUrl, supabaseAnonKey, {
+		db: {
+			schema: 'seismology'
+		}
+	});
 
-  const indices = getIndices(data);
+	const indices = getIndices(data);
 
-  const xmlReport = createXMLReport(data);
-  // await supabaseClient.from("survey").insert([
-  //   {
-  //     survey_data: data,
-  //     cws: indices.cws,
-  //     cii: indices.cii,
-  //     indices: indices,
-  //     xml_report: xmlReport,
-  //     seism_guid: data.seism,
-  //     image: data.image,
-  //   },
-  // ]);
+	const xmlReport = createXMLReport(data);
+	// await supabaseClient.from("survey").insert([
+	//   {
+	//     survey_data: data,
+	//     cws: indices.cws,
+	//     cii: indices.cii,
+	//     indices: indices,
+	//     xml_report: xmlReport,
+	//     seism_guid: data.seism,
+	//     image: data.image,
+	//   },
+	// ]);
 
-  await supabaseClient.rpc("addsurvey", {
-    survey_data: JSON.stringify(data),
-    cws: indices.cws,
-    cii: indices.cii,
-    indices: JSON.stringify(indices),
-    seism_guid: data.seism,
-    xml_report: xmlReport,
-    parroquia: data.parroquia,
-    territori: data.territori,
-    image: data.image ?? "",
-    lon: data.coordinates ? data.coordinates[0] : 0,
-    lat: data.coordinates ? data.coordinates[1] : 0,
-  });
+	await supabaseClient.rpc('addsurvey', {
+		survey_data: JSON.stringify(data),
+		cws: indices.cws,
+		cii: indices.cii,
+		indices: JSON.stringify(indices),
+		seism_guid: data.seism,
+		xml_report: xmlReport,
+		parroquia: data.parroquia,
+		territori: data.territori,
+		image: data.image ?? '',
+		lon: data.coordinates ? data.coordinates[0] : 0,
+		lat: data.coordinates ? data.coordinates[1] : 0
+	});
 };
 
 export const uploadImage = async (fileName: string, fileToUpload: string) => {
-  const supabaseClient = createClient(supabaseUrl, supabaseAnonKey, {
-    db: {
-      schema: "seismology",
-    },
-  });
-  const { data, error } = await supabaseClient.storage
-    .from("seismology")
-    .upload(fileName, fileToUpload);
+	const supabaseClient = createClient(supabaseUrl, supabaseAnonKey, {
+		db: {
+			schema: 'seismology'
+		}
+	});
+	const { data, error } = await supabaseClient.storage
+		.from('seismology')
+		.upload(fileName, fileToUpload);
 
-  const publicUrl = await supabaseClient.storage
-    .from("seismology")
-    .getPublicUrl(fileName);
+	const publicUrl = await supabaseClient.storage.from('seismology').getPublicUrl(fileName);
 
-  return publicUrl.data.publicUrl;
+	return publicUrl.data.publicUrl;
+};
+
+export interface SurveyData {
+	perceptionImage: string;
+}
+export interface Survey {
+	guid: string;
+	cws: number;
+	cii: number;
+	input_date: Date;
+	survey_data: SurveyData;
+}
+export const getSeismSurveys = async (seismGuid: string) => {
+	const supabaseClient = createClient(supabaseUrl, supabaseAnonKey, {
+		db: {
+			schema: 'seismology'
+		}
+	});
+
+	const { data: dataSeism, error: errorSeism } = await supabaseClient
+		.from('survey')
+		.select()
+		.eq('seism_guid', seismGuid);
+
+	errorSeism && console.log('Error downloading seisms:', errorSeism);
+	return (dataSeism ?? []) as Survey[];
 };
---
+++ src/routes/resums/+page.svelte
@@ -0,0 +1,134 @@
+<script lang="ts">
+	import {
+		getParroquiesData,
+		getSeismData,
+		getSeismSurveys,
+		type ParroquiaData,
+		type SeismsData,
+		type Survey
+	} from '$lib/fetch';
+	import { perceptionIndexImages } from '$lib/surveyObjects';
+	import Card from '@smui/card';
+	import DataTable, { Body, Cell, Head, Row } from '@smui/data-table';
+	import FormField from '@smui/form-field';
+	import Select, { Option } from '@smui/select';
+	import { onMount } from 'svelte';
+	import { format } from 'date-fns';
+	let seisms: SeismsData[] = [];
+	let parroquies: ParroquiaData[] = [];
+
+	let seism: string;
+
+	let surveys: Survey[] = [];
+
+	onMount(async () => {
+		seisms = await getSeismData();
+
+		parroquies = await getParroquiesData();
+	});
+	const getSurveys = async () => {
+		if (seism) surveys = await getSeismSurveys(seism);
+	};
+
+	$: seism, getSurveys();
+</script>
+
+<svelte:head>
+	<title>Enquesta sísmica</title>
+	<meta name="description" content="Enquesta sísmica Andorra Recerca i Innovació" />
+</svelte:head>
+<div class="header">
+	<img src="images/logo.png" alt="logo" class="logo" />
+	<div>
+		<div class="title">Enquesta sísmica</div>
+		<p>Resums de les enquestes</p>
+	</div>
+</div>
+<Card padded>
+	<div>Triï el sisme</div>
+	<FormField>
+		<Select style="min-width: 300px" label="Data del sisme" bind:value={seism}>
+			{#each seisms as seism}
+				<Option value={seism.guid}
+					>{`${new Date(seism.datetime).toLocaleDateString('ca-ES', {
+						weekday: 'long',
+						year: 'numeric',
+						month: 'long',
+						day: 'numeric',
+						timeZone: 'CET'
+					})} ${new Date(seism.datetime).toLocaleTimeString('ca-ES', {
+						hour: '2-digit',
+						minute: '2-digit',
+						timeZone: 'CET'
+					})} (${seism.region}). Mag:${seism.magnitude}`}</Option
+				>
+			{/each}
+		</Select>
+	</FormField>
+</Card>
+<DataTable>
+	<Head>
+		<Row>
+			<Cell>Data</Cell>
+			<Cell>Favorite Color</Cell>
+			<Cell>Percepció</Cell>
+		</Row>
+	</Head>
+	<Body>
+		{#each surveys as survey}
+			<Row>
+				<Cell>{format(new Date(survey.input_date), 'yyyy-MM-dd HH:mm')}</Cell>
+				<Cell>Favorite Color</Cell>
+				<Cell
+					><img
+						src={perceptionIndexImages.find((d) => d.value === survey.survey_data.perceptionImage)
+							?.imageLink}
+						alt="imatge percebuda"
+					/>
+				</Cell>
+			</Row>
+		{/each}
+	</Body>
+</DataTable>
+
+<style>
+	.header {
+		font-weight: bold;
+		font-family: Roboto, sans-serif;
+		padding-bottom: 10px;
+		display: flex;
+	}
+	.header img {
+		max-width: 210px;
+		max-height: 82px;
+		height: auto;
+		width: auto;
+	}
+
+	@media (max-width: 800px) {
+		.header {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+		}
+	}
+	@media (min-width: 800px) {
+		:global(.date-time) {
+			display: flex;
+			flex-direction: row;
+		}
+	}
+	.title {
+		font-size: 2em;
+	}
+	.logo {
+		max-height: 64px;
+	}
+	:global(.halign) {
+		display: flex;
+		flex-direction: row;
+	}
+	:global(.mdc-card) {
+		margin-top: 10px;
+	}
+</style>
